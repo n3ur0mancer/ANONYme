eface0b69a9c427cf58646b84741cba92460fef4
Kira Langeheine <kiralangeheine@Kiras-MBP.fritz.box>
2020-07-09 22:22:26 +0200

    Added the basic content for choosing the victim and styles as well as some images regarding ui button and link elements

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+router.get('/victim', function(req, res, next) {
+  res.render('victim', { title: 'Wähle dein Opfer' });
+});
+router.get('/game', function (req,res, ) {
+  res.render('game');
 });
 
 module.exports = router;
