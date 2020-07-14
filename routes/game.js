c201e45180656d7003cd4753c717b9f4c59d68d1
Kira Langeheine <kiralangeheine@Kiras-MBP.fritz.box>
2020-07-14 13:21:28 +0200

    updated index link to opponent, created game route

---
+++ routes/game.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const db = require('../db/index');
+
+const gameRouter = express.Router();
+
+gameRouter.get('/game', function(req, res, next) {
+    res.render('game');
+});
+
+module.exports = router;
