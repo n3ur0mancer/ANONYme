cf759d3f2648a4fec069fbaddaaeff3d500445c9
n3ur0mancer <46748400+n3ur0mancer@users.noreply.github.com>
2020-07-14 13:24:13 +0200

    Add files via upload

---
+++ public/scripts/clickevent.js
@@ -0,0 +1,7 @@
+console.log('code is running');
+
+const button = document.getElementById('thumbsdown');
+button.addEventListener('click', function(e) {
+    console.log('button was clicked');
+
+});
